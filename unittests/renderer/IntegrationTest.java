cb8f2ebd339c00c0db42462b683274941561ae7c
Israel_sha <054304hayim@gmail.com>
2024-06-10 00:02:57 +0300

    stage 4 begin

--- unittests/renderer/IntegrationTest.java
+++ unittests/renderer/IntegrationTest.java
@@ -2,6 +2,7 @@ package renderer;
 
 import geometries.Intersectable;
 import geometries.Sphere;
+import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
@@ -9,54 +10,59 @@ import primitives.Vector;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IntegrationTest {
 
-    /** Camera builder for the tests */
+    /**
+     * Camera builder for the tests
+     */
     private final Camera.Builder cameraBuilder = Camera.getBuilder()
             .setLocation(Point.ZERO)
             .setDirection(new Vector(0, 0, -1), new Vector(0, -1, 0))
             .setVpDistance(10);
     Camera camera = cameraBuilder.setVpSize(3, 3).build();
 
-    int sum = 0;
-
-    private LinkedList<Ray> constructRayOnAllPixels()
-    {
-        final var rays = new LinkedList<Ray>();
-        for(int i = 0; i < 3; i++) {
+    private LinkedList<Ray> constructRayOnAllPixels() {
+        final var listOfRays = new LinkedList<Ray>();
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                rays.add(camera.constructRay(3,3,j,i));
+                listOfRays.add(camera.constructRay(3, 3, j, i));
             }
         }
-        return rays;
+        return listOfRays;
     }
 
-    void testSphereInCenterOfPixel()
+   final private   LinkedList<Ray> listOfRays = constructRayOnAllPixels();
+
+    private int countIntersectionSphere(Sphere sphere)
     {
-        Sphere sphere = new Sphere(1,new Point(0,0,-3));
-        List<Point> temp;
-        for(Ray ray : constructRayOnAllPixels()) {
+        int sum=0;
+        for (Ray ray :listOfRays) {
+            List<Point> temp;
             temp = sphere.findIntersections(ray);
-            sum += temp == null? 0 : temp.size();
+            sum += temp == null ? 0 : temp.size();
         }
+        return sum;
     }
 
+    @Test
+    void testSphereIntegration() {
 
 
 
+        //TC01 intersection points in the case of a sphere of size 1 pixel and located in front of the center pixel
+        Sphere sphere1 = new Sphere(1, new Point(0, 0, -3));
+        assertEquals(2, countIntersectionSphere(sphere1),
+                "Fail to return 2 intersection points in the case of a sphere of size 1 pixel and located in front of the center pixel (TC01)");
 
+        //TC02 intersection points in the case of a sphere that in size of the view plan
+        camera = cameraBuilder.setLocation(new Point(0, 0, 0.5)).build();
+        Sphere sphere2 = new Sphere(2.5, new Point(0, 0, -2.5));
+       assertEquals(18,countIntersectionSphere(sphere2),
+               "Fail to return 18 intersection points in the case of a sphere that in size of the view plan (TC02) ");
 
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 }
